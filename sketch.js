d284d14cb886a3983fdc2414e457248c582bb218
rik1205 <112353046+rik1205@users.noreply.github.com>
2022-10-02 13:44:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var marks = [35, 38, 42, 45, 43, 34, 46, 41, 48, 32];
+
+//nested condition
+function setup() {
+  createCanvas(400, 400);
+  for (var i = 0; i < marks.length; i++) {
+
+    if (marks[i] > 45) {
+      console.log(marks[i]);
+    }
+
+  }
+}
+
+function draw() {
+  background(30);
+}
